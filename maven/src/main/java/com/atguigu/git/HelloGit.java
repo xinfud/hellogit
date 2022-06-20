cebbf44002ca004c6c60f0feda2bd364d2867b37
xiaoyang <2687166896@qq.com>
2022-06-20 15:45:47 +0800

    提交分支

--- maven/src/main/java/com/atguigu/git/HelloGit.java
+++ maven/src/main/java/com/atguigu/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hellogit");
+        System.out.println("dadsad");
     }
 }
